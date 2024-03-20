4b0cf83ddb65440f27baaa3590a51aee94ee6883
AlexisDeCachard <alexisdecachard66740@gmail.com>
2024-03-20 10:15:38 +0100

    cas 7 fini

--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -10,6 +10,20 @@ public class BoundaryAfficherMarche {
 	}
 
 	public void afficherMarche(String nomAcheteur) {
-
+		String[] infosMarche=controlAfficherMarche.donnerInfosMarche();
+		if (infosMarche.length==0) {
+			System.out.println("Le marché est vide, revenez plus tard.");
+		} else {
+			StringBuilder chaine = new StringBuilder();
+			chaine.append(nomAcheteur + ", vous trouverez dans ce marché :\n");
+			for (int i = 0; i < infosMarche.length; i++) {
+				chaine.append("- " + infosMarche[i]);
+				i++;
+				chaine.append(" qui vend " + infosMarche[i]);
+				i++;
+				chaine.append(" " + infosMarche[i] + ".\n");
+			}
+		System.out.println(chaine);
+		}
 	}
 }
